91acdd06d6701facf60142cb8ca6a4c558131012
Steve Astels <sastels@gmail.com>
2019-09-08 15:20:35 -0400

    rough in game ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Grommet } from 'grommet'
+import { Box, Grommet, Heading, Text } from 'grommet'
+import AppBar from './componants/AppBar'
 
 const theme = {
   global: {
@@ -12,7 +13,39 @@ const theme = {
 }
 
 const App = () => {
-  return <Grommet theme={theme}>Hello world!</Grommet>
+  return (
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level="1" size="small" margin="none">
+            Simon
+          </Heading>
+          <Text>Score: 99</Text>
+          <Text>Record: 99</Text>
+        </AppBar>
+
+        <Box direction="column" flex overflow={{ horizontal: 'hidden' }}>
+          <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+            <Box flex align="center" justify="center">
+              A
+            </Box>
+            <Box flex align="center" justify="center">
+              B
+            </Box>
+          </Box>
+
+          <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+            <Box flex align="center" justify="center">
+              C
+            </Box>
+            <Box flex align="center" justify="center">
+              D
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
+  )
 }
 
 export default App
