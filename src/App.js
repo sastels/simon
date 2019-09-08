752b4112f890159b6ac1a3bd6dda2fdacd219038
Steve Astels <sastels@users.noreply.github.com>
2019-09-08 14:58:03 -0400

    clean up code and add grommet (#1)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Grommet } from 'grommet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
 }
 
-export default App;
+const App = () => {
+  return <Grommet theme={theme}>Hello world!</Grommet>
+}
+
+export default App
