1c26dc806212bf0dae1a26e28a11e14126d8aec6
Steve Astels <sastels@gmail.com>
2019-09-08 19:03:39 -0400

    logic working

--- src/App.js
+++ src/App.js
@@ -36,47 +36,34 @@ const SimonButton = props => {
 const newRound = roundLength => {
   const items = ['A', 'B', 'C', 'D']
   const roundSequence = [...Array(roundLength)]
-    .map(() => items.splice(Math.floor(Math.random() * items.length), 1)[0])
+    .map(() => items[Math.floor(Math.random() * items.length)])
     .join('')
   console.log(roundSequence)
   return roundSequence
 }
 
 const App = () => {
-  const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
   const [sequence, setSequence] = useState('A')
   const [userSequence, setUserSequence] = useState('')
 
   const pressButton = letter => {
-    console.log(`pressed ${letter}`)
     const newUserSequence = userSequence.concat(letter)
-
-    console.log(`seq ${sequence}`)
-    console.log(`usr ${newUserSequence}`)
-
     if (sequence.slice(0, newUserSequence.length) === newUserSequence) {
-      console.log('-- 1 --')
       if (sequence.length === newUserSequence.length) {
-        console.log('-- 1 1 --')
         // won this round
-        const newScore = sequence.length
-        setBestScore(Math.max(newScore, bestScore))
-        const nextRound = newRound(newScore + 1)
+        setBestScore(Math.max(sequence.length, bestScore))
+        const nextRound = newRound(sequence.length + 1)
         setSequence(nextRound)
-        setScore(newScore)
         setUserSequence('')
       } else {
-        console.log('-- 1 2 --')
         // not done yet
         setUserSequence(newUserSequence)
       }
     } else {
-      console.log('-- 2 --')
       // lost round
-      setScore(0)
-      setUserSequence('')
       setSequence(newRound(1))
+      setUserSequence('')
     }
   }
 
@@ -87,8 +74,8 @@ const App = () => {
           <Heading level="1" size="small" margin="none">
             Simon
           </Heading>
-          <Text>Score: {score}</Text>
-          <Text>Record: {bestScore}</Text>
+          <Text size="xlarge">Current: {sequence.length}</Text>
+          <Text size="xlarge">Record: {bestScore}</Text>
         </AppBar>
 
         <Box direction="column" flex overflow={{ horizontal: 'hidden' }}>
