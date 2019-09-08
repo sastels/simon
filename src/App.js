76fdb64008a6e8b2822ae003faf0bb3fde7c57ab
Steve Astels <sastels@users.noreply.github.com>
2019-09-08 19:52:50 -0400

    rough in buttons (#3)
    * rough in buttons
    * logic working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import { Box, Grommet, Heading, Text } from 'grommet'
+import React, { useState } from 'react'
+import { Box, Button, Grommet, Heading, Text } from 'grommet'
 import AppBar from './componants/AppBar'
 
 const theme = {
   global: {
+    colors: {
+      brand: '#228BE6',
+    },
     font: {
       family: 'Roboto',
       size: '14px',
@@ -12,7 +15,58 @@ const theme = {
   },
 }
 
+const SimonButton = props => {
+  const { label, color, textColor, onClick } = props
+
+  return (
+    <Button
+      onClick={onClick}
+      label={
+        <Text color={textColor} size="xxlarge">
+          {label}
+        </Text>
+      }
+      primary
+      fill
+      color={color}
+    />
+  )
+}
+
+const newRound = roundLength => {
+  const items = ['A', 'B', 'C', 'D']
+  const roundSequence = [...Array(roundLength)]
+    .map(() => items[Math.floor(Math.random() * items.length)])
+    .join('')
+  console.log(roundSequence)
+  return roundSequence
+}
+
 const App = () => {
+  const [bestScore, setBestScore] = useState(0)
+  const [sequence, setSequence] = useState('A')
+  const [userSequence, setUserSequence] = useState('')
+
+  const pressButton = letter => {
+    const newUserSequence = userSequence.concat(letter)
+    if (sequence.slice(0, newUserSequence.length) === newUserSequence) {
+      if (sequence.length === newUserSequence.length) {
+        // won this round
+        setBestScore(Math.max(sequence.length, bestScore))
+        const nextRound = newRound(sequence.length + 1)
+        setSequence(nextRound)
+        setUserSequence('')
+      } else {
+        // not done yet
+        setUserSequence(newUserSequence)
+      }
+    } else {
+      // lost round
+      setSequence(newRound(1))
+      setUserSequence('')
+    }
+  }
+
   return (
     <Grommet theme={theme} full>
       <Box fill>
@@ -20,26 +74,46 @@ const App = () => {
           <Heading level="1" size="small" margin="none">
             Simon
           </Heading>
-          <Text>Score: 99</Text>
-          <Text>Record: 99</Text>
+          <Text size="xlarge">Current: {sequence.length}</Text>
+          <Text size="xlarge">Record: {bestScore}</Text>
         </AppBar>
 
         <Box direction="column" flex overflow={{ horizontal: 'hidden' }}>
           <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
             <Box flex align="center" justify="center">
-              A
+              <SimonButton
+                label="A"
+                color="red"
+                textColor="white"
+                onClick={() => pressButton('A')}
+              />
             </Box>
             <Box flex align="center" justify="center">
-              B
+              <SimonButton
+                label="B"
+                color="blue"
+                textColor="white"
+                onClick={() => pressButton('B')}
+              />
             </Box>
           </Box>
 
           <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
             <Box flex align="center" justify="center">
-              C
+              <SimonButton
+                label="C"
+                color="green"
+                textColor="white"
+                onClick={() => pressButton('C')}
+              />
             </Box>
             <Box flex align="center" justify="center">
-              D
+              <SimonButton
+                label="D"
+                color="yellow"
+                textColor="black"
+                onClick={() => pressButton('D')}
+              />
             </Box>
           </Box>
         </Box>
